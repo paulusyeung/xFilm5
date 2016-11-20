6436a2db1bbc62c02d0d55a72c4a5120db0b711d
paulusyeung <paulusyeung@nxstudio.com>
2016-11-20 15:47:21 +0800

    xFilm5.QRStation

---
+++ xFilm5.QRStation/Desktop.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace xFilm5.QRStation
+{
+    partial class Desktop
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.splitContainer2 = new System.Windows.Forms.SplitContainer();
+            this.picPreview = new System.Windows.Forms.PictureBox();
+            this.pnlInput = new System.Windows.Forms.Panel();
+            this.pnlContent = new System.Windows.Forms.Panel();
+            this.txtQrCodeData = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).BeginInit();
+            this.splitContainer2.Panel1.SuspendLayout();
+            this.splitContainer2.Panel2.SuspendLayout();
+            this.splitContainer2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picPreview)).BeginInit();
+            this.pnlInput.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.FixedPanel = System.Windows.Forms.FixedPanel.Panel1;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.pnlInput);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.splitContainer2);
+            this.splitContainer1.Size = new System.Drawing.Size(784, 561);
+            this.splitContainer1.SplitterDistance = 81;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // splitContainer2
+            // 
+            this.splitContainer2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer2.FixedPanel = System.Windows.Forms.FixedPanel.Panel1;
+            this.splitContainer2.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer2.Name = "splitContainer2";
+            // 
+            // splitContainer2.Panel1
+            // 
+            this.splitContainer2.Panel1.Controls.Add(this.pnlContent);
+            // 
+            // splitContainer2.Panel2
+            // 
+            this.splitContainer2.Panel2.Controls.Add(this.picPreview);
+            this.splitContainer2.Size = new System.Drawing.Size(784, 476);
+            this.splitContainer2.SplitterDistance = 261;
+            this.splitContainer2.TabIndex = 0;
+            // 
+            // picPreview
+            // 
+            this.picPreview.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.picPreview.Location = new System.Drawing.Point(0, 0);
+            this.picPreview.Name = "picPreview";
+            this.picPreview.Size = new System.Drawing.Size(519, 476);
+            this.picPreview.TabIndex = 0;
+            this.picPreview.TabStop = false;
+            // 
+            // pnlInput
+            // 
+            this.pnlInput.Controls.Add(this.txtQrCodeData);
+            this.pnlInput.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnlInput.Location = new System.Drawing.Point(0, 0);
+            this.pnlInput.Name = "pnlInput";
+            this.pnlInput.Size = new System.Drawing.Size(784, 81);
+            this.pnlInput.TabIndex = 0;
+            // 
+            // pnlContent
+            // 
+            this.pnlContent.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnlContent.Location = new System.Drawing.Point(0, 0);
+            this.pnlContent.Name = "pnlContent";
+            this.pnlContent.Size = new System.Drawing.Size(261, 476);
+            this.pnlContent.TabIndex = 0;
+            // 
+            // txtQrCodeData
+            // 
+            this.txtQrCodeData.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtQrCodeData.Font = new System.Drawing.Font("Courier New", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtQrCodeData.Location = new System.Drawing.Point(0, 0);
+            this.txtQrCodeData.Multiline = true;
+            this.txtQrCodeData.Name = "txtQrCodeData";
+            this.txtQrCodeData.ScrollBars = System.Windows.Forms.ScrollBars.Horizontal;
+            this.txtQrCodeData.Size = new System.Drawing.Size(784, 81);
+            this.txtQrCodeData.TabIndex = 0;
+            // 
+            // Desktop
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "Desktop";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "xFilm5 QR Station";
+            this.Load += new System.EventHandler(this.Desktop_Load);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.splitContainer2.Panel1.ResumeLayout(false);
+            this.splitContainer2.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).EndInit();
+            this.splitContainer2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.picPreview)).EndInit();
+            this.pnlInput.ResumeLayout(false);
+            this.pnlInput.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.SplitContainer splitContainer2;
+        private System.Windows.Forms.PictureBox picPreview;
+        private System.Windows.Forms.Panel pnlInput;
+        private System.Windows.Forms.TextBox txtQrCodeData;
+        private System.Windows.Forms.Panel pnlContent;
+    }
+}
+
---
+++ xFilm5.QRStation/Desktop.cs
@@ -0,0 +1,152 @@
+﻿using RestSharp;
+using RestSharp.Authenticators;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace xFilm5.QRStation
+{
+    public partial class Desktop : Form
+    {
+        #region private properties
+        private enum SourceType
+        {
+            None,
+            Blueprint,
+            Plate
+        }
+        #endregion
+
+        public Desktop()
+        {
+            InitializeComponent();
+
+            txtQrCodeData.KeyUp += TxtQrCodeData_KeyUp;
+        }
+
+        private void Desktop_Load(object sender, EventArgs e)
+        {
+            this.Text += " " + Properties.Settings.Default.BlueprintMachines;
+        }
+
+        private void TxtQrCodeData_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyValue == (char)Keys.Return)
+            {
+                e.Handled = true;
+
+                int barcodeLength = txtQrCodeData.TextLength;
+
+                txtQrCodeData.Select(0, barcodeLength);
+
+                String[] qrCodeData = txtQrCodeData.Text.Trim().Split(new string[] { "\n", "\r\n" }, StringSplitOptions.RemoveEmptyEntries);
+
+                // 2016.11.15 paulus: QR Code 有 3 lines,
+                //   line1 = Machine Type (藍紙 or 鋅板);
+                //   line2 = 時間;
+                //   line3 = Customer Number . Cups Job Id - File Name . p#(CMYK)
+                if (qrCodeData.Length == 3)
+                {
+                    SourceType sourceType = IdentifySourceType(qrCodeData[0]);
+
+                    String[] line3 = qrCodeData[2].Split('.');
+                    int clientId = Convert.ToInt32(line3[0]);
+                    String cupsJobId = line3[1].Split('-')[0];
+                    String sql = String.Format("ClientID = {0} AND CupsJobID = N'{1}'", clientId.ToString(), cupsJobId);
+                    //DAL.PrintQueue pq = DAL.PrintQueue.LoadWhere(sql);
+
+                    //GetClientInfo(clientId);
+                    //if (pq != null)
+                    //    GetPrintQLifeCycle(pq.ID);
+                }
+            }
+        }
+
+        private SourceType IdentifySourceType(String target)
+        {
+            SourceType result = SourceType.None;
+
+            //var bp = ConfigurationManager.AppSettings["BlueprintMachines"];
+            String bMachines = Properties.Settings.Default.BlueprintMachines;
+            String pMachines = Properties.Settings.Default.CtpMachines;
+
+            if (bMachines.ToUpper().Contains(target.ToUpper()))
+                result = SourceType.Blueprint;
+            else if (pMachines.ToUpper().Contains(target.ToUpper()))
+            {
+                result = SourceType.Plate;
+            }
+
+            return result;
+        }
+
+        private void GetClientInfo(int clientId)
+        {
+            //DAL.Client client = DAL.Client.Load(clientId);
+            //if (client != null)
+            //{
+
+            //}
+        }
+
+        private void GetPrintQLifeCycle(int printQueueId)
+        {
+            String sql = String.Format("PrintQueueId = {0}", printQueueId.ToString());
+            String[] orderBy = { "CreatedOn" };
+            //DAL.PrintQueue_LifeCycleCollection allCycle = DAL.PrintQueue_LifeCycle.LoadCollection(sql, orderBy, true);
+        }
+
+        #region RestSharp samples
+        private void Basic_xFilm5Api()
+        {
+            var client = new RestClient();
+            client.BaseUrl = new Uri("http://twitter.com");
+            client.Authenticator = new HttpBasicAuthenticator("username", "password");
+
+            var request = new RestRequest();
+            request.Resource = "statuses/friends_timeline.xml";
+
+            IRestResponse response = client.Execute(request);
+        }
+
+        private void Get_xFilm5Api()
+        {
+            var client = new RestClient("192.168.0.1");
+            var request = new RestRequest("api/item/", Method.GET);
+            //var queryResult = client.Execute<List<Items>>(request).Data;
+        }
+
+        private void Post_xFilm5Api()
+        {
+            var client = new RestClient("http://192.168.0.1");
+            var request = new RestRequest("api/item/", Method.POST);
+            request.RequestFormat = DataFormat.Json;
+            //request.AddBody(new Item
+            //{
+            //    ItemName = someName,
+            //    Price = 19.99
+            //});
+            client.Execute(request);
+        }
+
+        private void Delete_xFIlm5Api()
+        {
+            var client = new RestClient("http://192.168.0.1");
+            var request = new RestRequest("api/item/", Method.POST);
+            request.RequestFormat = DataFormat.Json;
+            //request.AddBody(new Item
+            //{
+            //    ItemName = someName,
+            //    Price = 19.99
+            //});
+            client.Execute(request);
+        }
+        #endregion
+    }
+}
