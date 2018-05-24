66b6d91da063b55796e1667aa5627470f462d512
paulus <paulusyeung@gmail.com>
2018-05-25 06:23:50 +0800

    Syntax fix

--- xFilm5.REST/Controllers/InvoiceController.cs
+++ xFilm5.REST/Controllers/InvoiceController.cs
@@ -57,6 +57,8 @@ select [ClientId]
       ,[CreatedBy]
       ,[LastModifiedOn]
       ,[LastModifiedBy]
+      ,[WorkshopId]
+      ,[WorkshopName]
 from (
 SELECT [ClientId]
       ,[ClientName]
@@ -77,6 +79,8 @@ SELECT [ClientId]
       ,[CreatedBy]
       ,[LastModifiedOn]
       ,[LastModifiedBy]
+      ,[WorkshopId]
+      ,[WorkshopName]
 	  ,row_number() OVER (partition BY ClientName ORDER BY ClientName, InvoiceNumber) AS Ln
 FROM [xFilm3_NuStar].[dbo].[vwInvoiceList_All]
 where ([ClientStatus] = 1) and ([InvoiceDate] >= '{0}') and (convert(nvarchar(7), InvoiceDate, 120) = '{1}')
@@ -122,6 +126,8 @@ SELECT TOP (1000) [ClientId]
       ,[CreatedBy]
       ,[LastModifiedOn]
       ,[LastModifiedBy]
+      ,[WorkshopId]
+      ,[WorkshopName]
 FROM [xFilm3_NuStar].[dbo].[vwInvoiceList_All]
 where ([ClientStatus] = 1) and ([InvoiceDate] >= '{0}') and (ClientId = {1}) and (convert(nvarchar(7), InvoiceDate, 120) = '{2}')
 order by [InvoiceNumber]", _DateZero.ToString("yyyy-MM-dd"), id.ToString(), month);
@@ -179,6 +185,8 @@ SELECT TOP (1000) [ClientId]
       ,[CreatedBy]
       ,[LastModifiedOn]
       ,[LastModifiedBy]
+      ,[WorkshopId]
+      ,[WorkshopName]
 FROM [xFilm3_NuStar].[dbo].[vwInvoiceList_All]
 where ([ClientStatus] = 1) and [InvoiceDate] >= '{0}' and ((convert(nvarchar, ClientId) like '%{1}%') or (ClientName like '%{1}%') or (InvoiceNumber like '%{1}%' or (convert(nvarchar(10), InvoiceDate, 120) like '%{1}%'))) and [WorkshopName] = N'{2}'
 order by [InvoiceNumber]", _DateZero.ToString("yyyy-MM-dd"), keyword, workshop);
@@ -205,6 +213,8 @@ SELECT TOP (1000) [ClientId]
       ,[CreatedBy]
       ,[LastModifiedOn]
       ,[LastModifiedBy]
+      ,[WorkshopId]
+      ,[WorkshopName]
 FROM [xFilm3_NuStar].[dbo].[vwInvoiceList_All]
 where ([ClientStatus] = 1) and [InvoiceDate] >= '{0}' and ((convert(nvarchar, ClientId) like '%{1}%') or (ClientName like '%{1}%') or (InvoiceNumber like '%{1}%' or (convert(nvarchar(10), InvoiceDate, 120) like '%{1}%')))
 order by [InvoiceNumber]", _DateZero.ToString("yyyy-MM-dd"), keyword);
@@ -234,6 +244,8 @@ SELECT TOP (1000) [ClientId]
       ,[CreatedBy]
       ,[LastModifiedOn]
       ,[LastModifiedBy]
+      ,[WorkshopId]
+      ,[WorkshopName]
 FROM [xFilm3_NuStar].[dbo].[vwInvoiceList_All]
 where ([ClientStatus] = 1) and [InvoiceDate] >= '{0}' and (ClientId = {1}) and ((ClientName like '%{2}%') or (InvoiceNumber like '%{2}%' or (convert(nvarchar(10), InvoiceDate, 120) like '%{2}%')))
 order by [InvoiceNumber]", _DateZero.ToString("yyyy-MM-dd"), id.ToString(), keyword);
